e4b144bb44161e693d0af7fb65c39b9e329bac25
deep1112002 <69797086+deep1112002@users.noreply.github.com>
2020-09-08 23:49:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import *
+import cv2
+app=Flask(__name__)
+
+@app.route('/')
+def starting():
+    return render_template('gogo.html')
+
+@app.route('/upload',methods=["POST"])
+def startit():
+    f=request.files['file']
+    f.save(f.filename)
+    img=cv2.imread(f.filename)
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return "Tu succesful hai BC "+f.filename
+
+if __name__=='__main__':
+    app.run(debug=True)
